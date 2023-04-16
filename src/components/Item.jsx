6c2eeeefda0637e7b01643e1a78e137b7d916fc4
C A B A L L E R O <caballerodev117@gmail.com>
2023-04-16 14:26:20 -0500

    item card style

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,17 +1,26 @@
 import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import ItemCount from './ItemCount';
 
 function Item({ info }) {
 
-
   return (
-    <>
+    <Card style={{ width: '18rem' }}>
       <Link to={`/detalle/${info.id}`} className='producto'>
-        <img src={info.img} height='150px' alt= {info.title} />
+      <Card.Img variant="top" src={info.img} alt= {info.title} />
       </Link>
-      <p>{info.title} {info.description} {info.price}</p>
-    </>
-
-  )
+      <Card.Body>
+        <Card.Title>{info.title}</Card.Title>
+        <Card.Text>
+          {info.description}
+          <p>${info.price}</p>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
 }
+
+
 
 export default Item;
