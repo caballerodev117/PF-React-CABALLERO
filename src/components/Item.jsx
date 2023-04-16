05129dcb4d25d381827c341a96b28c60c2947388
C A B A L L E R O <caballerodev117@gmail.com>
2023-04-16 12:48:38 -0500

    .

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -6,7 +6,7 @@ function Item({ info }) {
   return (
     <>
       <Link to={`/detalle/${info.id}`} className='producto'>
-        <img src={info.img} height='150px' width='150px' alt= {info.title} />
+        <img src={info.img} height='150px' alt= {info.title} />
       </Link>
       <p>{info.title} {info.description} {info.price}</p>
     </>
